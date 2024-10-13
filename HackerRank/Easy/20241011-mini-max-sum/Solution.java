5dedd74cac50968f7423c6fd2f94ec6bd8c28526
tsun-code <61623464+tsun-code@users.noreply.github.com>
2024-10-13 17:10:20 +1100

    Added Exercises

---
+++ HackerRank/Easy/20241011-mini-max-sum/Solution.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        int min = arr.get(0);
+        int max = arr.get(0);
+        long sum = 0;
+        for (int i=0; i < arr.size(); i++) {
+            int num = arr.get(i);
+            sum += num;
+            min = num < min ? num : min;
+            max = num > max ? num : max;
+        }
+        System.out.printf("%d %d%n",sum-max,sum-min);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
